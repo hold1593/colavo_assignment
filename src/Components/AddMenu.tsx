642e91ffad9bbe047a4d3e710ca2350ba511468a
mimi_cho <hold1593@naver.com>
2021-10-04 00:32:42 +0900

    [Feat] add discount list

--- src/Components/AddMenu.tsx
+++ src/Components/AddMenu.tsx
@@ -57,7 +57,9 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
   const [discountModal, setDiscountModal] = useState(false);
   const [totalPr, setTotalPr] = useState(0);
   const [arr, setArr] = useState<myProps['arr']>([]);
+  const [disArr, setDisArr] = useState<myProps['arr']>([]);
   let dumy: any[] = [...arr];
+  let dumyDis: any[] = [...disArr];
   
   const showSurgeryModal = () => {
     setSurgeryModal(true);
@@ -75,14 +77,14 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
     setDiscountModal(false);
   };
 
-  const handleChange = (value:string) => {
-    console.log(`selected ${value}`);
+  const handleChange = (e:any) => {
+    console.log(e.target)
   }
 
   const handlePickItem = (e:any) => {
     let data: {name:string, price:string} = {
       name: e.target.id,
-      price: e.target.name
+      price: e.target.name,
     }
     if(e.target.checked){ // 체크누르면
       dumy.push(data); // newArr에 넣자
@@ -94,6 +96,22 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
       }
     };
   }
+
+  const handlePickDiscount = (e:any) => {
+    let data: {name:string, rate:number} = {
+      name: e.target.id,
+      rate: e.target.name,
+    }
+    if(e.target.checked){ // 체크누르면
+      dumyDis.push(data); // newArr에 넣자
+    }else{ // 체크해제하면
+      for(let i=0; i<dumyDis.length; i++){
+        if(dumyDis[i]['name'] === e.target.id){ // newArr에 있는지 확인하구 있다면
+          dumyDis.splice(i,1) //없애자
+        }
+      }
+    };
+  }
   
   const handleOkSurgery = () => {
     setArr(dumy);
@@ -101,6 +119,7 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
   };
 
   const handleOkDiscount = () => {
+    setDisArr(dumyDis);
     setDiscountModal(false);
   };
 
@@ -109,6 +128,7 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
       <ButtonSection>
         <Button type="primary" onClick={showSurgeryModal} icon={<PlusOutlined />} >
           시술 추가
+          {console.log('arr::',arr)}
         </Button>
         <Modal title="시술 메뉴" visible={surgeryModal} onOk={handleOkSurgery} onCancel={handleCancelSurgery}>
           <ModalUl>
@@ -134,7 +154,7 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
             Object.entries(discount).map((e,idx) => {
               return(
                 <ModalLi key={idx}>
-                  <ModalInput type='checkbox' id={e[1]['name']}/>
+                  <ModalInput type='checkbox' id={e[1]['name']} name={e[1]['rate']} onClick={handlePickDiscount}/>
                   <label htmlFor={e[1]['name']}>{e[1]['name']}</label>
                   <p>{Math.floor(e[1]['rate']*100)}%</p>
                 </ModalLi>
@@ -147,14 +167,15 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
       <hr style={{ width: 500 }}/>
       <MenuList>
         {
+          arr.length > 0 ?
           arr.map((e,idx) => {
             return (
               <MenuBox key={idx}>
                 <MenuName>
                   <ItemName>{e.name}</ItemName>
-                  <Price >{e.price}원</Price>
+                  <Price>{e.price}원</Price>
                 </MenuName>
-                <Select defaultValue="1" style={{ width: 60 }} onChange={handleChange}>
+                <Select defaultValue="1" style={{ width: 60 }} onChange={handleChange} >
                   <Option value="1">1</Option>
                   <Option value="2">2</Option>
                   <Option value="3">3</Option>
@@ -164,6 +185,32 @@ const AddMenu = ({item, discount, currencyCode}: Props) => {
               </MenuBox>
             )
           })
+          :
+          <></>
+        }
+        {
+          disArr.length > 0 ?
+            disArr.map((e, idx) => {
+              return(
+                <MenuBox key={idx}>
+                <MenuName>
+                  <ItemName>{e.name}</ItemName>
+                  <Price>{Math.floor(e.rate*100)}%</Price>
+                </MenuName>
+                <Select defaultValue="선택" style={{ width: 80 }} >
+                  {arr.map((e,idx) => {
+                    return (
+                      <>
+                        <Option key={idx} value={idx}>{e.name}</Option>
+                      </>
+                    )
+                  })}
+                </Select>
+              </MenuBox>
+              )
+            })
+            :
+            <></>
         }
       </MenuList>
       <hr style={{ width: 500 }}/>
